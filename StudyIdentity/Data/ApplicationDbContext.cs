d3d36c00168ef46c61e5635e969d11ee2c20829d
JoaoVictorG <joaovictorgon36@gmail.com>
2022-09-09 16:15:54 -0300

    todos os arquivos cshtml do identity

---
+++ StudyIdentity/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StudyIdentity.Models;
+
+namespace StudyIdentity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<StudyIdentity.Models.User>? User { get; set; }
+    }
+}
